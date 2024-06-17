ad6de0cd9e7bb2c6a281b6cbe61657cce4ab1465
nmurphy <nmurphy.mine@gmail.com>
2024-06-17 10:24:24 -0700

    working with first_block

--- src/main/java/me/njbizzle/firstmod/FirstBlock.java
+++ src/main/java/me/njbizzle/firstmod/FirstBlock.java
@@ -1,14 +1,28 @@
 package me.njbizzle.firstmod;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.damagesource.DamageType;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.projectile.Projectile;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.portal.DimensionTransition;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.Vec3;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.function.ToIntFunction;
 
 public class FirstBlock extends Block
 {
+    public static final BooleanProperty LIGHT_UP = BooleanProperty.create("light_up");
+
     public FirstBlock()
     {
         super(BlockBehaviour.Properties.of()
@@ -17,12 +31,30 @@ public class FirstBlock extends Block
             .lightLevel(new ToIntFunction<BlockState>() {
                 @Override
                 public int applyAsInt(BlockState value) {
-                    return 5;
+                    return value.getValue(LIGHT_UP) ? 10 : 0;
                 }
             })
             .sound(SoundType.AMETHYST)
             .destroyTime(1)
-//            .lootFrom()
         );
+
+        this.registerDefaultState(
+            this.stateDefinition.any()
+                .setValue(LIGHT_UP, false)
+        );
+
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(LIGHT_UP);
+        super.createBlockStateDefinition(builder);
+    }
+
+    @Override
+    public void stepOn(@NotNull Level level, @NotNull BlockPos pos, BlockState state, Entity entity) {
+        entity.addDeltaMovement(new Vec3(1, 1, 1));
+        this.stateDefinition.any().setValue(LIGHT_UP, !state.getValue(LIGHT_UP));
+        super.stepOn(level, pos, state, entity);
     }
 }
