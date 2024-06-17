32305d8763177ec8b63d7a67d4ece2e31ab853f2
TEAM-Axel <theoluxleon@gmail.com>
2024-06-17 05:14:01 -0700

    Cleaned up code
    Removed "FirstBlock", was very confusing and need to add other blocks but was interfering.
    
    Added "raw_enderite" and "enderite_plating" in advance for armor
    
    need help in FirstMod.java, line 73, for some reason a part of the registry block of code I tried to make does not like "block" and refuses to accept it

--- src/main/java/me/njbizzle/firstmod/ModItem.java
+++
@@ -1,4 +0,0 @@
-package me.njbizzle.firstmod;
-
-public class ModItem {
-}
---
+++ src/main/java/me/njbizzle/firstmod/block/ModBlocks.java
@@ -0,0 +1,37 @@
+package me.njbizzle.firstmod.block;
+
+import me.njbizzle.firstmod.FirstMod;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.function.Supplier;
+
+public class ModBlocks {
+    public static final DeferredRegister<Block> BLOCKS =
+            DeferredRegister.create(ForgeRegistries.BLOCKS, FirstMod.MODID);
+
+    public static final RegistryObject<Block> SAPPHIRE_BLOCK = registerBlock("enderite_ore",
+            () -> new Block(BlockBehaviour.Properties.of().sound(SoundType.LODESTONE)));
+
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn);
+        return toReturn;
+    }
+
+    public static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block) {
+        return FirstMod.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+    }
+
+    public static void register(IEventBus eventBus) {
+        BLOCKS.register(eventBus);
+    }
+}
