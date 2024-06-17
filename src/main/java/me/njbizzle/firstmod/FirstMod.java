e433d67a0576c46d1fc900189939a98ce3a5f5d3
nmurphy <nmurphy.mine@gmail.com>
2024-06-17 15:43:36 -0700

    working on loot

--- src/main/java/me/njbizzle/firstmod/FirstMod.java
+++ src/main/java/me/njbizzle/firstmod/FirstMod.java
@@ -29,75 +29,47 @@ import org.slf4j.Logger;
 
 import java.util.function.Supplier;
 
-import static me.njbizzle.firstmod.FirstMod.ModBlocks.registerBlock;
-
 // The value here should match an entry in the META-INF/mods.toml file
 
 @Mod(FirstMod.MODID)
 public class FirstMod
 {
-// Define mod id in a common place for everything to reference
-public static final String MODID = "firstmod";
-// Directly reference a slf4j logger
-private static final Logger LOGGER = LogUtils.getLogger();
-// Create a Deferred Register to hold Blocks which will all be registered under the "firstmod" namespace
-public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
-// Create a Deferred Register to hold Items which will all be registered under the "firstmod" namespace
-public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
-// Create a Deferred Register to hold CreativeModeTabs which will all be registered under the "firstmod" namespace
-public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "firstmod";
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    // Create a Deferred Register to hold Blocks which will all be registered under the "firstmod" namespace
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
+    // Create a Deferred Register to hold Items which will all be registered under the "firstmod" namespace
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
+    // Create a Deferred Register to hold CreativeModeTabs which will all be registered under the "firstmod" namespace
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
 
 
-public static final RegistryObject<Item> ENDERITE_BAR = ITEMS.register("enderite_bar",
-        () -> new Item(new Item.Properties().stacksTo(64)));
-public static final RegistryObject<Item> RAW_ENDERITE = ITEMS.register("raw_enderite",
-        () -> new Item(new Item.Properties().stacksTo(64)));
-public static final RegistryObject<Item> ENDERITE_PLATING = ITEMS.register("enderite_plating",
-        () -> new Item(new Item.Properties().stacksTo(64)));
+    public static final RegistryObject<Item> ENDERITE_BAR = ITEMS.register("enderite_bar",
+            () -> new Item(new Item.Properties().stacksTo(64)));
+    public static final RegistryObject<Item> RAW_ENDERITE = ITEMS.register("raw_enderite",
+            () -> new Item(new Item.Properties().stacksTo(64)));
+    public static final RegistryObject<Item> ENDERITE_PLATING = ITEMS.register("enderite_plating",
+            () -> new Item(new Item.Properties().stacksTo(64)));
 
+    public static final RegistryObject<Block> ENDERITE_ORE = registerBlock("enderite_ore",
+            () -> new Block(BlockBehaviour.Properties.of()));
+    public static final RegistryObject<Block> FIRST_BLOCK = registerBlock("first_block", FirstBlock::new);
 
+    public static final RegistryObject<CreativeModeTab> ENDERITE_TAB = CREATIVE_MODE_TABS.register("enderite_tab", () -> CreativeModeTab.builder()
+        .withTabsBefore(CreativeModeTabs.COMBAT)
+        .icon(() -> ENDERITE_BAR.get().getDefaultInstance())
+        .displayItems(
+            (parameters, output) -> {
+                ITEMS.getEntries().forEach(item -> output.accept(item.get()));
+            }
+        )
+        .build()
+    );
 
-
-
-
-public static final RegistryObject<Block> ENDERITE_ORE = registerBlock("enderite_ore");
-
-public class ModBlocks {
-    public static final DeferredRegister<Block> BLOCKS =
-            DeferredRegister.create(ForgeRegistries.BLOCKS, FirstMod.MODID);
-
-    private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, Supplier<T> block) {
-        return FirstMod.ITEMS.register(name, () -> new BlockItem(block.get(),new Item.Properties()));
-    }
-    public static <T extends Block> RegistryObject<T> registerBlock(String name) {
-        RegistryObject<T> toReturn = BLOCKS.register(name, );
-        registerBlockItem(name, toReturn);
-        return toReturn;
-    }
-    public static void register(IEventBus eventBus) {
-        BLOCKS.register(eventBus);
-    }
-}
-
-
-
-
-    // Creates a creative tab with the id "firstmod:example_tab" for the example item, that is placed after the combat tab
-    public static final RegistryObject<CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("example_tab", () -> CreativeModeTab.builder()
-            .withTabsBefore(CreativeModeTabs.COMBAT)
-            .icon(() -> ENDERITE_BAR.get().getDefaultInstance())
-            .displayItems(
-                    (parameters, output) -> {
-//                output.accept(EXAMPLE_ITEM.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
-//                output.accept(EXAMPLE_BLOCK_ITEM.get());
-                        output.accept(ENDERITE_BAR.get());
-                        output.accept(RAW_ENDERITE.get());
-                        output.accept(ENDERITE_PLATING.get());
-                    }
-            )
-            .build());
-
-    {
+    public FirstMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         // Register the commonSetup method for modloading
@@ -116,44 +88,51 @@ public class ModBlocks {
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
-
-        // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
-//        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
+
+    private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, Supplier<T> block) {
+        return ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+    }
+
+    public static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn);
+        return toReturn;
+    }
+
+    public static void register(IEventBus eventBus) {
+        BLOCKS.register(eventBus);
+    }
+
 
     private void commonSetup(final FMLCommonSetupEvent event) {
         // Some common setup code
         LOGGER.info("HELLO FROM COMMON SETUP");
-
-//        if (Config.logDirtBlock)
-//            LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT));
-//
-//        LOGGER.info(Config.magicNumberIntroduction + Config.magicNumber);
-//
-//        Config.items.forEach((item) -> LOGGER.info("ITEM >> {}", item.toString()));
     }
 
-    // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS)
-        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS)
-            event.accept(RAW_ENDERITE);
-            event.accept(ENDERITE_PLATING);
-            event.accept(ENDERITE_BAR);
-            event.accept(ENDERITE_ORE);
+        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
+            event.accept(FIRST_BLOCK.get());
+        }
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(RAW_ENDERITE.get());
+            event.accept(ENDERITE_PLATING.get());
+            event.accept(ENDERITE_BAR.get());
+            event.accept(ENDERITE_ORE.get());
+        }
     }
 
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event) {
-        // Do something when the server starts
         LOGGER.info("HELLO from server starting");
     }
 
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
     @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents {
+
         @SubscribeEvent
         public static void onClientSetup(FMLClientSetupEvent event) {
             // Some client setup code
